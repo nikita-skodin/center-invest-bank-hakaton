b7fc060b7d8579b9c1e7a6facca67f075ba15e6b
Master <mileker.2487@mail.ru>
2023-10-25 20:36:05 +0300

    a

---
+++ src/main/java/com/bank/service/AddressService.java
@@ -0,0 +1,33 @@
+package com.bank.service;
+
+import com.bank.dto.AddressDTO;
+import com.bank.exceptions.NotFoundException;
+import com.bank.exceptions.ResourceNotFoundException;
+import com.bank.models.Address;
+import com.bank.repositories.AddressRepository;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AddressService {
+    private final AddressRepository addressRepository;
+
+    public AddressService(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+    public AddressDTO getAddressBy(String address){
+        Optional<AddressDTO> optional= addressRepository.findAddress(address);
+        return addressRepository.findAddress(address).orElseThrow(()
+                -> new NotFoundException("Address with this id not found!"));
+    }
+
+    public void save (Address address){
+        //TODO String KOORDINATI = getKoordinati;
+        //KOORDINATI.setKOORDINATI()
+    }
+
+
+}
