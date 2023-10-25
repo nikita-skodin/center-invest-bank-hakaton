da481a103f73c0a36a0064f612e3169f00caac03
Nikita Skodin <skodin36@gmail.com>
2023-10-25 15:34:11 +0300

    1

---
+++ src/main/java/com/bank/dto/ErrorDTO.java
@@ -0,0 +1,15 @@
+package com.bank.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ErrorDTO {
+    String error;
+    String message;
+}
