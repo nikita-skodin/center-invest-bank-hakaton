d3bfc7eb4de8110d14e365600e1bd8ccbf1340d9
Master <mileker.2487@mail.ru>
2023-10-26 15:24:35 +0300

    a

---
+++ src/main/java/com/bank/repositories/LandmarkRepository.java
@@ -0,0 +1,22 @@
+package com.bank.repositories;
+
+import com.bank.models.Landmark;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface LandmarkRepository extends JpaRepository<Landmark,Long> {
+    List<Landmark> findAllByAddress_Address(String address);
+
+    List<Landmark> findAllByTitle(String title);
+
+    Optional<Landmark> findByTitle(String title);
+    Optional<Landmark> findById(int id);
+
+
+
+
+}
